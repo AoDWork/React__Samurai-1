217d116b51db2fdbb33a8c041b66e4d6c953c5cf
AoDWork <aodtest07@gmail.com>
2022-10-04 21:50:54 +0300

    50 home (49 bug - don't setUsers - chek function)

--- src/Components/Users/UsersContainer.jsx
+++ src/Components/Users/UsersContainer.jsx
@@ -4,7 +4,7 @@ import {connect} from 'react-redux';
 import {followAC, unfollowAC, setUsersAC} from '../redux/users-reducer'
 
 
-let mapDispatchToProps = (state) => {
+let mapStateToProps = (state) => {
     return {
         users: state.usersPage.usersData
     }
