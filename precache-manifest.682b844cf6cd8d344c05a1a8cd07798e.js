61cb38c294033c6d2e6b82ea41c66a0a26bc26c5
AoDWork <aodtest07@gmail.com>
2023-06-19 21:20:19 +0300

    Updates

---
+++ precache-manifest.682b844cf6cd8d344c05a1a8cd07798e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8e112000ff2f516d043a5e51fff5ced",
+    "url": "/React__Samurai-1/index.html"
+  },
+  {
+    "revision": "b81b788ef9bd4c7f0004",
+    "url": "/React__Samurai-1/static/css/3.48bb2f18.chunk.css"
+  },
+  {
+    "revision": "e2b3485f5e2e039323cb",
+    "url": "/React__Samurai-1/static/css/4.96075cd8.chunk.css"
+  },
+  {
+    "revision": "e81aaa127e10aeabb9f7",
+    "url": "/React__Samurai-1/static/css/main.122b44c5.chunk.css"
+  },
+  {
+    "revision": "fd78a3c2a0de86affa1d",
+    "url": "/React__Samurai-1/static/js/2.6fa82757.chunk.js"
+  },
+  {
+    "revision": "b81b788ef9bd4c7f0004",
+    "url": "/React__Samurai-1/static/js/3.6203e72f.chunk.js"
+  },
+  {
+    "revision": "e2b3485f5e2e039323cb",
+    "url": "/React__Samurai-1/static/js/4.140cacd8.chunk.js"
+  },
+  {
+    "revision": "e81aaa127e10aeabb9f7",
+    "url": "/React__Samurai-1/static/js/main.fb708f9e.chunk.js"
+  },
+  {
+    "revision": "2eeeab71917cbfffccfb",
+    "url": "/React__Samurai-1/static/js/runtime~main.6978c5f3.js"
+  },
+  {
+    "revision": "b08921bf6a852753d619f4bdd17c1247",
+    "url": "/React__Samurai-1/static/media/preloader.b08921bf.svg"
+  },
+  {
+    "revision": "62387d2a29d675d1435669aad6a4a512",
+    "url": "/React__Samurai-1/static/media/user.62387d2a.jpg"
+  }
+]);
